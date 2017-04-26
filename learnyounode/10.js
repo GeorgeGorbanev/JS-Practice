264260928b45c7ea50c04ee12817a08fe7f32068
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2017-04-26 10:25:59 +0300

    learn you node 10,11

--- learnyounode/10.js
+++ learnyounode/10.js
@@ -3,7 +3,24 @@ var net = require('net');
 var port = Number(process.argv[2]);
 
 server = net.createServer(function (socket) {
-    socket.end('');
+    socket.end(formatedDateTime() + '\n');
 });
 
-server.listen();
+server.listen(port);
+
+function formatedDateTime() {
+    var date = new Date();
+    var YYYY = date.getFullYear(),
+        MM   = formatInt(date.getMonth()+1, 2),
+        DD   = formatInt(date.getDate(), 2),
+        HH   = formatInt(date.getHours(), 2),
+        mm   = formatInt(date.getMinutes(), 2);
+    return`${YYYY}-${MM}-${DD} ${HH}:${mm}`
+}
+
+function formatInt(int, length) {
+    var intString = int.toString();
+    while (intString.length < length)
+        intString = '0' + intString;
+    return intString;
+}
